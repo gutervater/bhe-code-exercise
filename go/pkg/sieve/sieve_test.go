80a16605959011d92fbbc4d06c0e146377f64dd4
Sean G <maddenfreak622@yahoo.com>
2025-02-27 09:13:25 -0500

    updating tests to capture a couple edge cases

--- go/pkg/sieve/sieve_test.go
+++ go/pkg/sieve/sieve_test.go
@@ -3,12 +3,14 @@ package sieve
 import (
 	"math/big"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNthPrime(t *testing.T) {
 	sieve := NewSieve(SieveInitOptions{})
+	assert.Equal(t, int64(0), sieve.NthPrime(-1))
 	assert.Equal(t, int64(2), sieve.NthPrime(0))
 	assert.Equal(t, int64(71), sieve.NthPrime(19))
 	assert.Equal(t, int64(541), sieve.NthPrime(99))
@@ -18,7 +20,10 @@ func TestNthPrime(t *testing.T) {
 	assert.Equal(t, int64(15485867), sieve.NthPrime(1000000))
 	assert.Equal(t, int64(179424691), sieve.NthPrime(10000000))
 	assert.Equal(t, int64(2038074751), sieve.NthPrime(100000000))
+	// repeated calls should be fast
+	startTime := time.Now()
 	assert.Equal(t, int64(2038074751), sieve.NthPrime(100000000))
+	assert.LessOrEqual(t, time.Since(startTime).Milliseconds(), int64(50), "Cached call took too long")
 }
 
 func FuzzNthPrime(f *testing.F) {
